697d8536db73b349c56c5f354ea4365b915d8eb0
Troy McCall <troyxmccall@gmail.com>
2026-04-18 11:16:32 -0400

    add the actual entrypoint troy

---
+++ docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -eu
+
+SOCKET_PATH="${DOCKER_SOCKET_PATH:-/var/run/docker.sock}"
+
+if [ -S "$SOCKET_PATH" ]; then
+  socket_gid="$(stat -c '%g' "$SOCKET_PATH")"
+
+  if [ "$socket_gid" != "0" ]; then
+    socket_group="$(awk -F: -v gid="$socket_gid" '$3 == gid { print $1; exit }' /etc/group)"
+
+    if [ -z "$socket_group" ]; then
+      socket_group="dockersock"
+      addgroup -g "$socket_gid" -S "$socket_group"
+    fi
+
+    addgroup node "$socket_group" >/dev/null 2>&1 || true
+  fi
+fi
+
+exec su-exec node "$@"
